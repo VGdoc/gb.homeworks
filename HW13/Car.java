b212c996d12855b9e44eb06f07bd73620546aa35
Vladimir Grigoriev <intvreporting@gmail.com>
2022-01-14 18:23:46 +0200

    recomit hw13

--- HW13/Car.java
+++ HW13/Car.java
@@ -1,2 +1,47 @@
-package HW13;public class Car {
+package HW13;
+
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+    private Race race;
+    private int speed;
+    private String name;
+
+    public Car(Race race, int speed) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = Main.RESET + "\033[0;3" + CARS_COUNT%8 + "m" + //ставим цвет по кругу, стираем старую разметку и красим всю строку
+                "Участник #" + CARS_COUNT;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+            Main.waitAllOthers();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+            if ( i + 1 == race.getStages().size()){ ///// если последний трек в заезде
+                if (!race.getStages().get(i).isHasWinner()){ // если этот трек не имеет победителя
+                    race.getStages().get(i).setHasWinner(true); //изменяем значение
+                    System.out.printf("\033[0;93m" + "\nПОЗДРАВЛЯЕМ ПОБЕДИТЕЛЯ!\n***%s " + "\033[0;93m" + "пришёл первым!***\n\n" + Main.RESET,getName());
+                }
+                Main.waitAllOthers();
+            }
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
 }
--- HW13/Stage.java
+++ HW13/Stage.java
@@ -1,2 +1,21 @@
-package HW13;public class Stage {
+package HW13;
+
+public abstract class Stage {
+    protected int length;
+    protected String description;
+    private boolean hasWinner;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract void go(Car c);
+
+    public boolean isHasWinner() {
+        return hasWinner;
+    }
+
+    public void setHasWinner(boolean hasWinner) {
+        this.hasWinner = hasWinner;
+    }
 }
